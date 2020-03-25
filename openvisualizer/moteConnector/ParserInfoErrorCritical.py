316419f357a7bd24a04804c82003ca61487ed390
changtengfei <tengfei.chang@gmail.com>
2020-03-25 15:15:29 +0100

    parsing sixtop return code message in text.

--- openvisualizer/moteConnector/ParserInfoErrorCritical.py
+++ openvisualizer/moteConnector/ParserInfoErrorCritical.py
@@ -65,7 +65,7 @@ class ParserInfoErrorCritical(Parser.Parser):
         else:
             self.errorinfo[(callingComponent, error_code)] = 1
 
-        if error_code == 36:
+        if StackDefines.errorDescriptions[error_code].startswith('sixtop return code'):
             # replace args of sixtop command/return code id by string
             arg1 = StackDefines.sixtop_returncode[arg1]
             arg2 = StackDefines.sixtop_statemachine[arg2]
